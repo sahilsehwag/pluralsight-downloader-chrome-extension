ad6f211a18b9e937ca16a13e960f2e4d415c0566
richardbang83 <richard.bang83@gmail.com>
2020-09-08 01:21:17 +0900

    Fixed error
    Variable name is fixed on which was named incorrectly by previous commit.

--- background.js
+++ background.js
@@ -14,7 +14,7 @@ var onChangeFactory = responseCb => ({ state }) => {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'download' || request.action === 'download-sync') {
-    filename = request.filePath;
+    filePath = request.filePath;
 
     flag = true;
     try {
