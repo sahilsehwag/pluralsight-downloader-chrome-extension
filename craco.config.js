eda9b10e350c197208f50320107b3541014ece61
Sahil Sehwag <sehwagsahil002@gmail.com>
2023-08-07 14:11:59 +0530

    feat: updated manifest as per manifest v3

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
+	style: {
+		postcss: {
+			plugins: [require('tailwindcss'), require('autoprefixer')],
+		},
+	},
 }
